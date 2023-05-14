fb2a25ae791084ebdbb49355139ecb3599598035
Akos <kocsis.akos@students.jedlik.eu>
2023-05-14 10:03:34 +0200

    carousel bg

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-// color picker
-let left = document.getElementById("colorpicker").style.color;
-let right = document.getElementById("row");
-// console.log(left, right);
-var num = left.replace(/rgb\(([\s\S]+?)\)/g, "$1");
-console.log(num);
-var num2 = num.replace(/\s/g, "");
-var array = num2.split(",");
-console.log(array)
-array.forEach(valueToHex);
-function valueToHex(c) {
+var div = document.getElementsByClassName('row');
+var colorArray = [].slice.call(div);
 
-    var hex = c.toString(16);
-    
-    console.log(hex);
-    return hex
-}
+
+colorArray.forEach(split => {
+
+    console.log(split)
+
+});
+
+
+colorArray.forEach(array => {
+    array.style.backgroundColor = array.dataset.gradColor
+});
+
